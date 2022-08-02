9c3e20fd9e6bcc4ed0f6144d9788007bd07c859f
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-02 09:11:17 -0600

    Add attributes to ingredient class

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,18 +1,18 @@
 require './lib/ingredient'
 
-Rspec.describe Ingredient do
+describe Ingredient do
     let(:ingredient1) { Ingredient.new({ name: "Cheese", unit: "oz", calories: 50 }) }
-    
+    let(:ingredient2) { Ingredient.new({ name: "Maraconi", unit: "oz", calories: 200 }) }
 
     it 'has a name' do
+        expect(ingredient1.name).to eq "Cheese"
     end
 
-    xit 'has a unit' do
+    it 'has a unit' do
+        expect(ingredient1.unit).to eq "oz"
     end
 
-    xit 'has calories' do
+    it 'has calories' do
+        expect(ingredient1.calories).to eq 50
     end
-
-
-
 end
