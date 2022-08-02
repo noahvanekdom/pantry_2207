7849a5a2761450a7bdece43666a7d4b96868d444
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-02 09:06:33 -0600

    Initial commit

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,2 +1,4 @@
 class Ingredient
+
+
 end
---
+++ spec/ingredient_spec.rb
@@ -0,0 +1,18 @@
+require './lib/ingredient'
+
+Rspec.describe Ingredient do
+    let(:ingredient1) { Ingredient.new({ name: "Cheese", unit: "oz", calories: 50 }) }
+    
+
+    it 'has a name' do
+    end
+
+    xit 'has a unit' do
+    end
+
+    xit 'has calories' do
+    end
+
+
+
+end
